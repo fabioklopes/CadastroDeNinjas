c2dadfb1f4af31bec04045b37cfbc9deb2e654a4
Fábio Klevinskas Lopes <fabioklopes@live.com>
2025-05-13 11:36:13 -0300

    Reestruturação do Git para retomar as atualizações.

---
+++ src/main/java/br/dev/codeon/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -0,0 +1,15 @@
+package br.dev.codeon.CadastroDeNinjas.Ninjas;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/boas-vindas")
+    public String boasVindas() {
+        return "Essa é a minha primeira mensagem nessa rota.";
+    }
+
+}
